99d83ac01c85f0510604157d3834f876f6086192
Burkie11 <sam.fitz.burke@gmail.com>
2018-03-18 17:42:24 +1100

    Update tweet searching to include words and centred around melbounre with radius

--- start.js
+++
@@ -1,8 +0,0 @@
-require('dotenv').config({ path: 'variables.env' });
-
-const app = require('./app');
-app.set('port', process.env.PORT || 3000);
-const server = app.listen(app.get('port'), () => {
-  console.log(`Express running → PORT ${server.address().port}`);
-});
-
