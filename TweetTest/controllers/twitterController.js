bef7418263bad8e3f56767a60752e1aeb751a7d7
Burkie11 <sam.fitz.burke@gmail.com>
2018-03-18 17:41:50 +1100

    Update tweet searching to include words and centred around melbounre with radius

--- TweetTest/controllers/twitterController.js
+++ TweetTest/controllers/twitterController.js
@@ -1,5 +1,5 @@
 var Twitter = require('twitter');
-
+const moment = require('moment');
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -11,6 +11,31 @@ exports.test = async(req,res) =>
 {
 	tweets = ""
 	res.render('index', {data: tweets});
+}
+
+exports.getTweets = async(req,res) => 
+{
+	var params = {
+			q: req.body.word, 
+			result_type: "recent", 
+			geocode: `-37.8136,144.9631,${req.body.dist || 10}km`,
+			count: 100,
+		};
+	console.log(params);
+	client.get('search/tweets', params, function(error, tweets, response) 
+	{
+	  	if (!error) 
+	  	{
+	  		for(tweet in tweets.statuses)
+	  		{
+	  			tweets.statuses[tweet].created_at = moment(tweets.statuses[tweet].created_at).startOf('hour').fromNow(); 
+	  		}
+	    	res.render('index', {data: tweets.statuses})
+	  	} else
+	  	{
+	  		res.send(error)
+	  	}
+	});
 }
 
 exports.getTweetsForUser = async(req,res) => 
--- TweetTest/routes/index.js
+++ TweetTest/routes/index.js
@@ -6,5 +6,6 @@ const twitterController = require("../controllers/twitterController");
 /* GET home page. */
 router.get('/', twitterController.test)
 router.post('/getTwitterName', twitterController.getTweetsForUser)
+router.post('/getTweets', twitterController.getTweets)
 
 module.exports = router;
