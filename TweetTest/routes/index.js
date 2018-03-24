be96297356fafff67eae1cc19e30b5eaa93380d7
Ming Sheng <quahmingsheng@me.com>
2018-03-24 17:09:49 +1100

    Added route for /auto

--- TweetTest/routes/index.js
+++ TweetTest/routes/index.js
@@ -5,6 +5,8 @@ const twitterController = require("../controllers/twitterController");
 const dbpediaController = require("../controllers/dbpediaController");
 /* GET home page. */
 router.get('/', twitterController.test)
+router.get('/auto', twitterController.autoGet)
+router.post('/auto', twitterController.autoPost)
 router.post('/getTweets', dbpediaController.getDataMuse, twitterController.getTweets)
 router.get('/fliterTweets', twitterController.getFilterTweets)
 router.get('/getDBPedia', dbpediaController.getDBPedia)
