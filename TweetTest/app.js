7ff88e7e88cd4ee5f8d0f4b3b11e3bc494082e11
Burkie11 <sam.fitz.burke@gmail.com>
2018-03-25 15:49:32 +1100

    fix

--- TweetTest/app.js
+++ TweetTest/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var helmet = require('helmet')
 var index = require('./routes/index');
 
 var app = express();
@@ -20,6 +20,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(helmet());
 
 app.use('/', index);
 
