169d96e9655a97f7de04ae9448b838bc5b9f693b
Ming Sheng <quahmingsheng@me.com>
2018-03-26 15:32:59 +1100

    Updated the POST command to make it more reliable

--- TweetTest/controllers/autoController.js
+++ TweetTest/controllers/autoController.js
@@ -9,7 +9,7 @@ function collect(query)
     request.post({
         headers: {'content-type' : 'application/x-www-form-urlencoded'},
         url:     'http://localhost:3000/getTweets',
-        body:    "word=" + query
+        form:    { wprd: query,shouldStoreTweets: true }
     }, function(error, response, body){
         //console.log(err);
     });
