cb82b258bcfdf0158684355915c1e5e6d106042e
tripaloski <jason.97.liew@gmail.com>
2018-04-18 19:30:35 +1000

    Install dependencies & config Jest

--- frontend/__tests__/App.test.js
+++ frontend/__tests__/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import App from './App';
-
+import NativeBase from 'native-base';
+import ReactNavigation from 'react-navigation'
+import App from '../App';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
