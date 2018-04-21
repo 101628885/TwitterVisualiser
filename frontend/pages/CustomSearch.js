62c035acea9930b140b79ce754efe585d6cdbb1c
coreyjenkins <101101551@student.swin.edu.au>
2018-04-21 22:48:15 +1000

    Switched Picker with ActionSheet in CustomSearch

--- frontend/pages/CustomSearch.js
+++ frontend/pages/CustomSearch.js
@@ -7,20 +7,20 @@
 import React from 'react';
 import {
   Text,
-  StyleSheet,
   View
 } from 'react-native';
 import {
-  Picker,
-  Content,
   Button,
-  Icon
+  Icon,
+  ActionSheet
 } from "native-base";
 import {
   Font,
   Constants
 } from 'expo';
 
+var BUTTONS = ["10", "20", "30", "Cancel"];
+var CANCEL_INDEX = 3;
 export default class CustomSearch extends React.Component {
   constructor(props) {
     super(props);
@@ -46,34 +46,30 @@ export default class CustomSearch extends React.Component {
   //returns a view with a button that opens a dialog to change the tweets
   render() {
     return (
-      <View style={{ borderWidth: 0 }}>
-        <Button transparent>
+      <View>
+        <Button transparent
+          onPress={() =>
+            ActionSheet.show(
+              {
+                options: BUTTONS,
+                cancelButtonIndex: CANCEL_INDEX,
+                title: "Select Amount of Tweets"
+              },
+              buttonIndex => {
+                if (buttonIndex === 3) {
+                  return;
+                }
+                else {
+                  this.setState({
+                    tweetValue: BUTTONS[buttonIndex],
+                  }, () => this.props.action(this.state.tweetValue));
+                }
+              }
+            )}
+        >
           <Icon name="ios-list-box" />
         </Button>
-        <Picker
-          mode="dialog"
-          placeholder="Choose tmount of tweets"
-          placeholderStyle={{ color: "#2874F0" }}
-          note={false}
-          style={style.pickerStyle}
-          selectedValue={this.state.tweetValue}
-          onValueChange={this.changeTweetValue.bind(this)}
-          prompt="Choose amount of tweets"
-        >
-          <Picker.Item label="10 tweets" value="10" />
-          <Picker.Item label="20 tweets" value="20" />
-          <Picker.Item label="30 tweets" value="30" />
-        </Picker>
       </View>
     )
   }
 }
-
-const style = StyleSheet.create({
-  pickerStyle: {
-    position: 'absolute',
-    top: 0,
-    width: 1000,
-    height: 1000,
-  },
-})
