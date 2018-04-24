7ec3c8f10587132d7e56e41e7ea0579db6f53386
Ming Sheng <quahmingsheng@me.com>
2018-04-24 21:52:40 +1000

    Route for re checking

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 const twitterController = require("../controllers/twitterController");
 const dbpediaController = require("../controllers/dbpediaController");
 const verifyController = require("../controllers/verifyController");
+const reverifyController = require("../controllers/reverificationController");
 const reactController = require("../controllers/reactController");
 const nlpTrainingController = require("../controllers/nlpTrainingController");
 
@@ -23,6 +24,11 @@ router.get('/bulk', twitterController.getBulkTweetsOld)
 
 //Dpedia Controller
 router.get('/getDBPedia', dbpediaController.getDBPedia)
+
+//Re-verify Controller
+router.get('/temp', reverifyController.getUncheckedTweets)
+router.post('/temp/:id/:value', reverifyController.checkTweets)
+router.post('/temp/:id/:value/:location/:type', reverifyController.checkTweets)
 
 //Verify Controller
 router.get('/check', verifyController.getUncheckedTweets)
