21f3165c902488f1a19e1e390cea4aa9dc1ab75b
Shane Joachim <shanejoachim41@gmail.com>
2018-04-28 20:20:01 +1000

    Returns all checked tweets + performance improvements

--- backend/controllers/nlpTrainingController.js
+++ backend/controllers/nlpTrainingController.js
@@ -3,10 +3,12 @@ const mongoose = require('mongoose');
 const tweet = require('../models/tweet_schema');
 var db = mongoose.connection;
 
-exports.returnNLPDataSet = async (req,res) =>
+// Returns relevant data based on the params value
+exports.returnNLPDataSetCount = async (req,res) =>
 {
     var tweet = db.model('tweets', tweet);
-    tweet.find({checked: true}).sort({'date': -1}).limit(parseInt(req.params.count)).skip(Math.floor((Math.random() * 50) + 1)).lean().exec(function(err, posts)
+    
+    await tweet.find({checked: true}).sort({'date': -1}).limit(parseInt(req.params.count)).skip(Math.floor((Math.random() * 50) + 1)).lean().exec(function(err, posts)
     {
         if(!err)
         {
@@ -31,6 +33,35 @@ exports.returnNLPDataSet = async (req,res) =>
         }
         else
         {
+            console.log(err);
+        }
+    });
+}
+
+// Returns all relevant data
+exports.returnNLPDataSet = async (req,res) => {
+    var tweet = db.model('tweets', tweet);
+    
+    await tweet.find({checked: true}).lean().exec(function(err, posts) {
+        if(!err) {
+            let count = 0;
+            let post;
+            let newRes = new Array();
+            
+            for (let i in posts) {
+                post = {
+                    "full_text": posts[count].full_text,
+                    "crime": posts[count].crime,
+                    "type_of_crime" : posts[count].type_of_crime,
+                    "location" : posts[count].location
+                };
+
+                count += 1;
+                newRes.push(post);
+            }
+
+            res.send(newRes);
+        } else {
             console.log(err);
         }
     });
