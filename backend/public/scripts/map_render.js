ee5aee0f94254a0a28ace9a0ff8289e48eb9b544
shanejoachim <shanejoachim41@gmail.com>
2018-08-20 00:42:47 +1000

    :sparkles: :construction: contains trajectory algorithm logic

--- backend/public/scripts/map_render.js
+++ backend/public/scripts/map_render.js
@@ -1,5 +1,6 @@
 let historicCoords = {}
 let gMarkers = [];
+let trajectoryData = {}
 dataMap= null;
 var histCentre = [41.881832, -87.623177]
 
@@ -7,6 +8,11 @@ function setHistoricCoords(coords, centre)
 {
   historicCoords = coords;
   histCentre = centre;
+}
+
+function setTrajectoryData(data)
+{
+	trajectoryData = data;
 }
 
 function initMap() {
@@ -60,7 +66,7 @@ function initHistoricMap()
 	});
 
 	var infoWin = new google.maps.InfoWindow();
-	var markers = historicCoords.map(function(val) 
+	var markers = `historicCoords`.map(function(val) 
 	{
 		if(val.location != undefined && val.location.coordinates[0] !=null && val.location.coordinates[1] !=null)
 		{
@@ -81,6 +87,49 @@ function initHistoricMap()
 		}
 	});
 	gMarkers = markers
+}
+
+initPOCTrajectoryAlgorithm = () =>
+{	
+	console.log(trajectoryData);
+	console.log("yp", trajectoryData.data[0].coordinates);
+
+	let crimeCategories = [[]];
+
+	let defaultRegion = new google.maps.LatLng( 41.881832, -87.623177);
+	let n = 48;
+	let dt = trajectoryData.data[0].time;
+	let tTimeThreshold = 180;
+	let tDistThreshold = 500;
+	
+	for(var i in trajectoryData.data) {
+		//TODO: Tomorrow :P
+	}
+
+	// let lat1 = trajectoryData.data[0].coordinates[1]; //change to first latitude coord
+	// let lon1 = trajectoryData.data[0].coordinates[0]; //change to first longitude coord
+	// let lat2 = trajectoryData.data[1].coordinates[1]; //change to second latitude coord
+	// let lon2 = trajectoryData.data[1].coordinates[0]; //change to second longitude coord	 
+	dataMap = new google.maps.Map(
+	document.getElementById('map'), 
+	{
+		zoom: 15, 
+		center: defaultRegion,
+		mapTypeId: google.maps.MapTypeId.ROADMAP
+	});
+
+
+
+	//Calc Distance in terms of metres between Two Points
+	let R = 6371e3;
+	let φ1 = lat1 * Math.PI / 180;
+	let φ2 = lat2 * Math.PI / 180;
+  let λ1 = (lat2 - lat1) * Math.PI / 180;
+	let λ2 = (lon2 - lon1) * Math.PI / 180;
+	let a = Math.sin(λ1/2) * Math.sin(λ1/2) + Math.cos(φ1) * Math.cos(φ2) * Math.sin(λ2) * Math.sin(λ2);
+	let c = 2 * Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+	let d = R*c;
+	console.log("THE D:", d);
 }
 
 function hideMarkers(crime)
