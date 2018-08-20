903c374fd1aea79490e2ebc67ac5b9f7f8b82b48
FleetAdmiralButter <101162341@student.swin.edu.au>
2018-08-20 13:02:09 +1000

    Updated city radius

--- backend/test/mongoTest.js
+++ backend/test/mongoTest.js
@@ -145,3 +145,82 @@ describe('storeTweets()', function(){
 });
 
 //create a new test to check that correct tweets are being written to the right DB...
+
+/*WIP
+describe('storeTweets()', function(){
+	it('should write a tweet to the DB specified', async()=>{
+
+		//SETUP, create 2 separate tweets
+		let sampleTweetMelb = {
+			"created_at": new Date("2017-03-24 15:15:06.000"),
+			"id": "1111",
+			"full_text": "Melbourne Test Tweet",
+			"user_name": "test_user",
+			"user_location": "test_loc",
+			"user_verified": true,
+			"user_profile_image_url": "http://sample.url/img.png",
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"checked": false,
+			"crime": null
+		};
+
+		let sampleTweetChicago = {
+			"created_at": new Date("2018-03-24 13:15:06.000"),
+			"id": "2222",
+			"full_text": "Chicago Test Tweet",
+			"user_name": "test_user2",
+			"user_location": "test_loc",
+			"user_verified": true,
+			"user_profile_image_url": "http://sample.url/img.png",
+			"geo": null,
+			"coordinates": null,
+			"place": null,
+			"checked": false,
+			"crime": null
+		};
+
+
+
+		let melbArray = [];
+		let chicagoArray = [];
+
+		melbArray.push(sampleTweetMelb);
+		chicagoArray.push(sampleTweetChicago);
+
+
+		//ACTION
+		mongo.storeTweets(melbArray, "melbourne");
+		mongo.storeTweets(chicagoArray, "chicago");
+
+		await timeout(2700);
+
+		let melbTweetFromMelbDB = {};
+		let melbTweetFromChicagoDB = {};
+		let chicagoTweetFromChicagoDB = {};
+		let chicagoTweetFromMelbDB = {};
+
+		await tweetMelb.find({id: "1111"}).lean().exec().then((res) => {melbTweetFromMelbDB = res}); //Try to get melb tweet from Melb DB
+		await tweetMelb.find({id: "2222"}).lean().exec().then((res) => {chicagoTweetFromMelbDB = res}); //Try to get chicago tweet from Melb DB
+		await tweetChicago.find({id: "2222"}).lean().exec().then((res) => {chicagoTweetFromChicagoDB = res}); //Try to get chicago tweet from Chicago DB
+		await tweetChicago.find({id: "1111"}).lean().exec().then((res) => {melbTweetFromChicagoDB = res}); //Try to get melb tweet from Chicago DB
+
+		await tweetMelb.remove({id: "1111"}).exec();
+
+		await tweetChicago.remove({id: "2222"}).exec();
+
+
+
+		//ASSERT
+		expect(melbTweetFromMelbDB.full_text).to.be.equal(sampleTweetMelb.full_text);
+		expect(chicagoTweetFromMelbDB.full_text).to.be.equal(undefined);
+
+		expect(chicagoTweetFromChicagoDB.full_text).to.be.equal(sampleTweetChicago.full_text);
+		expect(melbTweetFromChicagoDB.full_text).to.be.equal(undefined);
+
+
+
+	})
+});
+*/
