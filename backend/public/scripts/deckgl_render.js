2cb35265cabc1749108f3419186ae78906fe0228
tripaloski <jason.97.liew@gmail.com>
2018-09-15 18:36:06 +1000

    :zap: update deck.gl map to display plain trajectory geojson in scatterplot and line layers

--- backend/public/scripts/deckgl_render.js
+++ backend/public/scripts/deckgl_render.js
@@ -4,7 +4,8 @@ const INITIAL_VIEW_STATE = {
 	latitude: 41.88,
 	longitude: -87.62,
 	zoom: 11,
-	pitch: 40.5,
+	pitch: 50,
+	bearing: 0,
 };
 
 const deckgl = new deck.DeckGL({
@@ -18,14 +19,16 @@ const deckgl = new deck.DeckGL({
 });
 
 let chicago_tweet_data = null;
+let chicago_trajectories_data = null;
+let highway_data = null;
 
 // Array of options available to the user
 // radius in metres
 const OPTIONS = ['radius'];
 
-const DATA_URLS = {
+const DATA_URL = {
 	chicago_hist: 'https://data.cityofchicago.org/resource/6zsd-86xi.json',
-	chicago_tweets: 'https://api.myjson.com/bins/unbgw',  // temp api for tweets while the db was down
+	chicago_trajectories: 'http://api.myjson.com/bins/1b6z54',  // temp trajectories api
 }
 
 // for hex layers
@@ -71,19 +74,36 @@ const renderLayer = () => {
 		...options
 	});
 
+	const chicagoTrajectoriesLayer = new deck.GeoJsonLayer({
+		id: 'chicago-trajectories-layer',
+		data: chicago_trajectories_data,
+		stroked: true,
+		lineWidthMinPixels: 3,
+		lineJointRounded: true,
+		getFillColor: d => [160, 160, 180, 200],
+		getLineColor: d => [255, 255, 255, 255],
+		getRadius: d => 20,
+		radiusMinPixels: 20,
+	});
+
 	deckgl.setProps({
-		layers: [chicagoTweetLayer]
+		layers: [chicagoTrajectoriesLayer]
 	});
 }
 
 /**
  * initialiseData() initialises the global data variables by fetching data from the endpoints
- * specified in DATA_URLS. Called once only.
+ * specified in DATA_URL. Called once only.
  */
 const initialiseData = async() => {
-	const response = await fetch(DATA_URLS.chicago_tweets);
-	const json = await response.json();
-	chicago_tweet_data = json.map(datum => (datum.coordinates[0].coordinates));
+	// const response = await fetch(DATA_URL.chicago_tweets);
+	// const json = await response.json();
+	// chicago_tweet_data = json.map(datum => (datum.coordinates[0].coordinates));
+
+	const response = await fetch(DATA_URL.chicago_trajectories);
+	json = await response.json();
+	chicago_trajectories_data = json;
+
 	renderLayer();
 };
 
