53b5b00d49ca91a00888a2166cebf7526334f8f2
FleetAdmiralButter <101162341@student.swin.edu.au>
2018-09-28 02:29:21 +1000

    Automatic installer fully working for Ubuntu

--- backend/install.sh
+++ backend/install.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
@@ -7,7 +8,8 @@ fi
 echo "Welcome to the VisCrime automatic installation script."
 echo "If anything goes wrong, please email me at 101162341@student.swin.edu.au"
 
-if [ "$(uname)" == "Darwin" ]; then
+unamestr=`uname`
+if [[ "$unamestr" == 'Darwin' ]]; then
     echo "Setting up VisCrime for macOS..."  
     echo "Getting dependencies..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
@@ -16,19 +18,19 @@ if [ "$(uname)" == "Darwin" ]; then
     brew install node
     tar -xvzf VisCrime.tar.gz
     mv VisCrime ~/VisCrime
-    cd ~/VisCrime
+    cd ~/VisCrime/backend
     npm install
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+elif [[ "$unamestr" == 'Linux' ]]; then
     echo "Setting up VisCrime for Ubuntu..."
     echo "Getting dependencies..."
     apt-get update
     apt-get upgrade
+    curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+    sudo apt-get install -y nodejs
     apt-get install mongodb
-    apt-get install npm
-    apt-get install nodejs
     tar -xvzf VisCrime.tar.gz
     mv VisCrime ~/VisCrime
-    cd ~/VisCrime
+    cd ~/VisCrime/backend
     npm install
 
 fi
