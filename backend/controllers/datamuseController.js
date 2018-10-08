c61107c21a2f1dffc41e44e5e6674314f9d97b75
Stefan Tsvetkov <101628885@student.swin.edu.au>
2018-10-09 00:10:46 +1100

    Removing redundant functions, controllers and views

---
+++ backend/controllers/datamuseController.js
@@ -0,0 +1,74 @@
+const sparql = require('sparql');
+const request = require('request');
+const test = require('./cacheController');
+
+//https://www.datamuse.com/api/
+exports.getDataMuse = async(req, res, callback) =>
+{
+    let wordQuery = req.body.word;
+
+    if (wordQuery == "")
+    {
+        wordQuery = "crime";
+    }
+
+	options =
+		{
+			method: "GET",
+			url: `https://api.datamuse.com/words?rel_trg=` + wordQuery
+		}
+
+    request(options, function (error, response, body)
+    {
+        if (!error)
+        {
+            let words = []
+            let labels = wordQuery + " OR ";
+            words.push(wordQuery);
+            for (let i = 0; i < 8; i++)
+            {
+                if (body == "[]")
+                {
+                    break;
+                }
+
+                labels += JSON.parse(body)[i].word;
+                words.push(JSON.parse(body)[i].word);
+                if (i != 7)
+                {
+                    labels += " OR " ;
+                }
+            }
+            let result = {
+                words : words,
+                labels : labels
+            };
+
+            callback(result);
+
+        } else
+        {
+            res.send(JSON.parse(error));
+        }
+    });
+}
+
+//Gets results from DataMuse, creates an array and build the query to Twitter
+exports.getCombination = async (req, res, next) =>
+{
+    let result;
+    exports.getDataMuse(req, res, function (dataMuseResults) { //get results fom DataMuse
+
+        result = dataMuseResults;
+
+        result.labels += " AND -filter:retweets AND -filter:replies";
+
+        req.body.dbResults = result.labels;
+        req.body.words = result.words;
+
+        next();
+    });
+
+
+}
+
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,32 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-// TODO: clean these up
-const twitterController       = require("../controllers/twitterController");
 const listviewController      = require("../controllers/listViewController");
-const dbpediaController       = require("../controllers/dbpediaController");
+const datamuseController       = require("../controllers/datamuseController");
 const lookupController        = require("../controllers/lookupController");
 const verifyController        = require("../controllers/verifyController");
 const nlpTrainingController   = require("../controllers/nlpTrainingController");
 const autoController          = require("../controllers/autoController");
 const tweetMapController      = require("../controllers/tweetMapController");
-const reactController         = require("../controllers/reactController");
-const historicCrimeController = require("../controllers/historicCrimeController");
-const visualisationController = require("../controllers/visualisationController");
-
-// const mongoController      = require("../controllers/mongoController");
-// const cacheController      = require("../controllers/cacheController");
-const chicagoDataFactory      = require("../controllers/chicagoDataFactory");
-const chicago_data_factory    = require("../controllers/chicagoDataFactory");
 
 // get homepage
 router.get('/', function(req, res) {
   res.render('home');
-});
-
-// get about page
-router.get('/about', function(req, res) {
-  res.render('about');
 });
 
 // get deckgl map
@@ -34,21 +19,16 @@ router.get('/deckmap', function(req, res) {
   res.render('deckmap');
 });
 
-// twitter controller
-router.get('/browseTwitter', twitterController.browseTwitter);
-router.get('/getTweets', twitterController.browseTwitter);
-router.post('/getTweets', dbpediaController.getCombination, twitterController.getTweets);
-
 /**
  * lookup controller routes
  * handles the returning of lists of tweets to display on the lookup view
  */
 router.get('/lookup', lookupController.getAPITweetsView);
-router.post('/lookup', dbpediaController.getCombination, lookupController.getAPITweetsView);
+router.post('/lookup', datamuseController.getCombination, lookupController.getAPITweetsView);
 router.get('/lookup/api', lookupController.getAPITweetsView);
-router.post('/lookup/api', dbpediaController.getCombination, lookupController.getAPITweetsView);
+router.post('/lookup/api', datamuseController.getCombination, lookupController.getAPITweetsView);
 router.get('/lookup/db', lookupController.getDBTweetsView);
-router.post('/lookup/db', dbpediaController.getCombination, lookupController.getDBTweetsView);
+router.post('/lookup/db', datamuseController.getCombination, lookupController.getDBTweetsView);
 
 // auto controller
 router.get('/auto', autoController.autoGet);
@@ -59,31 +39,9 @@ router.get('/check', verifyController.getUncheckedTweets);
 router.post('/check/:id/:value/:geo', verifyController.checkTweets);
 router.post('/check/:id/:value/:location/:type/:geo', verifyController.checkTweets);
 
-// nlp training controller
-//router.get('/nlpTrainingEndpoint/:count/:crime', nlpTrainingController.returnNLPDataSet);  //returns crime: true or false
-//router.get('/nlpTrainingEndpoint/:count/', nlpTrainingController.returnNLPDataSet);  //returns both checked and unchecked
-
 //new endpoints for NLP
 router.get('/nlpTraining/:location/:count/checked/:checked', nlpTrainingController.returnNLPData);
 router.get('/nlpTraining/:location/:count/crime/:crime', nlpTrainingController.returnNLPData);
-
-
-/**
- * react controller routes
- * handles all interactions inteded for the react native mobile app
- * don't remove these, i think some parts in the web app actually use these
- */
-router.get('/getCrimeWordCount', reactController.getCrimeWordCount);
-router.get('/getPredictedData', reactController.getPredictedData);
-
-/**
- * visualisation controller routes
- * handles the supply of d3 data
- * i don't think we're using these anymore? we should delete :)
- */
-router.get('/visualisation', visualisationController.getVisualisation);
-router.get('/visualisationData', visualisationController.getVisualisationData);
-router.get('/nlpData', visualisationController.getNLPData);
 
 /**
  * tweetmap controller routes
@@ -95,19 +53,5 @@ router.post('/tweetMap', tweetMapController.queryMapData);
 // listview Controller
 router.get('/list', listviewController.listTweets);
 router.post('/list', listviewController.findTweets);
-
-/**
- * historiccrime controller routes
- * handles the supply of data from various historical records from various u.s. cities
- */
-router.get('/chicago', historicCrimeController.chicagoHandler); 
-
-router.get('/checkData', chicago_data_factory.checkLocalData); 
-router.get('/fixMapData', chicago_data_factory.fixMapData); 
-
-// test dummy data
-router.post('/trajectoryData', chicagoDataFactory.getDummyData);
-router.get('/tweetsChicagoWithLocation', chicagoDataFactory.getChicagoTweetsWithLocation);
-router.get('/saveDummy', chicagoDataFactory.saveCrimeData);
 
 module.exports = router;
