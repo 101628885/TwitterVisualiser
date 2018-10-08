d6db1036d80eda2fbb1411ddaecec7f3de922b3a
Stefan Tsvetkov <101628885@student.swin.edu.au>
2018-10-08 21:04:00 +1100

    Outdated frontend and Kepler map removed

--- frontend/__tests__/CustomSearch.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import CustomSearch from '../pages/CustomSearch';
-import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<CustomSearch />).toJSON();
-  expect(rendered).toBeTruthy();
-});
--- frontend/__tests__/Home.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Home from '../pages/Home';
-import renderer from 'react-test-renderer';
-
-it('renders without crashing', () => {
-  const rendered = renderer.create(<Home />).toJSON();
-  expect(rendered).toBeTruthy();
-});
