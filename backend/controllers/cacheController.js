96933abd30c72752408d5d52918e34b0efa82bb3
FleetAdmiralButter <101162341@student.swin.edu.au>
2018-10-09 19:11:28 +1100

    Fixed memory leak and expiry checking not working properly

--- backend/controllers/cacheController.js
+++ backend/controllers/cacheController.js
@@ -2,7 +2,7 @@ const request = require('request-promise');
 const factory = require('./tweetMapController')
 const timeout = ms => new Promise(res => setTimeout(res, ms));
 let cachedRequestList = []; //members: url, contents{}, expiry
-const cacheExpiryTime = 3600000; //1 hour
+const cacheExpiryTime = 10000; //1 hour
 let trajectoryCacheState = { cachedTrajectories: [], isBuilding: false };
 
 exports.getJSON = async (options) => {
@@ -56,10 +56,12 @@ exports.getTrajectories = async (req, res) => {
     for (let i = 0; i < trajectoryCacheState.cachedTrajectories.length; i++) {
         if (trajectoryCacheState.cachedTrajectories[i].query == query) //found item
         {
-            if (new Date() - trajectoryCacheState.cachedTrajectories.refreshedTime > cacheExpiryTime) { //expired, rebuild
+            if ((new Date() - trajectoryCacheState.cachedTrajectories[i].refreshedTime) > cacheExpiryTime) { //expired, rebuild
                 console.log("Trajectory data cache expired, rebuilding...");
                 result = await refreshTrajectoryData();
+                trajectoryCacheState.cachedTrajectories.splice(i, 1); //delete expired element
                 trajectoryCacheState.cachedTrajectories.push(result);
+
             }
             else
             {
