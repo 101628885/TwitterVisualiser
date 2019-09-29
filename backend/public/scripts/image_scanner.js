6b1680b96d6e9f39c950b00a89907db667cc26e6
BC <sxvitdzu@nbox.notif.me>
2019-09-29 18:43:58 +1000

    FEATURE: Add image presentation to image scanner

--- backend/public/scripts/image_scanner.js
+++ backend/public/scripts/image_scanner.js
@@ -4,4 +4,15 @@ async function scanImage(){
 			document.getElementById('result').innerHTML = JSON.stringify(json, undefined, 4);
 		})
 	})
+	displayImage(document.querySelector('input[type=file]').files[0]);
+}
+
+function displayImage(input){
+	const reader = new FileReader();
+    reader.readAsDataURL(input);
+    reader.onload = function(e) {
+    	const image = document.getElementById('imageDisplay');
+    	image.src = e.target.result;
+    	image.hidden = false;
+    };
 }
