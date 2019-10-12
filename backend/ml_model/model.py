d3ed9684383d0980d0ccf3b0b766ad071d39ff76
rohit <101547854@student.swin.edu.au>
2019-10-13 00:35:16 +1100

    FIX: Added combined model prediction

--- backend/ml_model/model.py
+++ backend/ml_model/model.py
@@ -5,7 +5,9 @@ import numpy as np
 from fastai.vision import *
 import argparse
 from sklearn.feature_extraction.text import CountVectorizer
-import pickle as pk
+import pickle
+from sklearn import svm
+
 
 def image_prediction(image_path):
 	test_src = (ImageList.from_csv("",'cleaned_trinary_test_30.csv', folder='images/25kimages')
@@ -25,15 +27,16 @@ def image_prediction(image_path):
 
 	    
 	y = learn.predict(open_image(image_path),thresh=0.2)
+	image_result = y[1].numpy()
 	image_prob = y[2].numpy()
 
-	return y[0],image_prob
+	return image_result,image_prob
 
 
 def rekognition_prediction(object_label):
 
 	with open('model.pickle', 'rb') as saved_model:
-		clf_from_pickle = pk.load(saved_model)
+		clf_from_pickle = pickle.load(saved_model)
 
 	df = pd.read_csv("cleaned_trinary_training_70.csv")
 	df = df[pd.notnull(df['object_labels'])]
@@ -46,9 +49,22 @@ def rekognition_prediction(object_label):
 	rekog_pred = clf_from_pickle.predict(count_vect.transform([str(object_label)])) 
 	rekog_prob = clf_from_pickle.predict_proba(count_vect.transform([object_label]))
 
-	return rekog_pred[0],rekog_prob[0]
+	label_dict = {'HighCrime':0, 'LowCrime':1, 'NoCrime': 2} #converting prediction into numeric array
+	pred_arr =  [0,0,0]
+	pred_arr[label_dict[rekog_pred[0]]] = 1
 
+	return pred_arr,rekog_prob[0]
 
+def svm_combined_model(image_result,rekog_pred):
+	filename = 'svm.pkl'
+	svm = pickle.load(open(filename, 'rb'))
+
+	x = np.vstack((image_result, rekog_pred)).T
+	nx, ny = x.shape
+	x = x.reshape((nx*ny))
+	result = svm.predict([x])
+
+	return result[0]
 
 
 def main():
@@ -65,10 +81,13 @@ def main():
 	#image_prediction
 	image_result,image_prob = image_prediction(image_path)
 
+	#rekog_prediction
 	rekog_pred,rekog_prob = rekognition_prediction(object_label)
 
-	print(image_result,image_prob)
-	print(rekog_pred,rekog_prob)
+	#cobined_model_prediction
+	result = svm_combined_model(image_result,rekog_pred)
+
+	print(result)
 
 
 if __name__ == "__main__":
